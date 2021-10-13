96ac1256d3509e6c9b5247801da674708369c1e1
Vladimir <stesh@ukr.net>
2021-10-14 02:01:19 +0300

    db sqllite connection init

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,16 @@
 import React from "react";
-import {View, StyleSheet, FlatList} from 'react-native';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
 import {Post} from "./Post";
 
-export const PostList = ({data, onOpen}) => {
+export const PostList = ({data = [], onOpen}) => {
+    if(!data.length) {
+        return (
+            <View style={styles.wrapper}>
+                <Text style={styles.noItems}>Постов еще нет</Text>
+            </View>
+        )
+    }
+
     return (
         <View style={styles.wrapper}>
             <FlatList
@@ -20,5 +28,11 @@ export const PostList = ({data, onOpen}) => {
 const styles = StyleSheet.create({
     wrapper: {
         padding: 10
+    },
+    noItems: {
+        fontFamily: 'open-regular',
+        textAlign: "center",
+        marginVertical: 10,
+        fontSize: 18
     }
 });
--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -4,7 +4,7 @@ import {DATA} from "../../data";
 export const loadPosts = () => {
     return {
         type: LOAD_POSTS,
-        payload: DATA
+        payload: []
     }
 }
 
